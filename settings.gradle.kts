237dbbd8c09e8b749aab3aace3083d0b3357363b
arslanefimov <dns230717rus@gmail.com>
2026-04-19 18:50:00 +0300

    added multimodule structure

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "itmo-work-backend-lab3"
+include("application-service")
+include("company-service")
+include("config-server")
+include("eureka-server")
+include("itmo-work-gateway")
+include("user-service")
+include("vacancy_service")
